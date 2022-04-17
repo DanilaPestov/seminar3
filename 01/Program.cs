3b9893fd606d9d47a85b47808440ee7509e577e0
DanilaPestov <89147750075@mail.ru>
2022-04-17 17:32:26 +1000

    01

---
+++ 01/Program.cs
@@ -0,0 +1,24 @@
+﻿int x, y;
+x = new Random().Next(-100, 330);
+y = new Random().Next(-164, 304);
+Console.WriteLine($"x={x}, y={y}");
+if (x == 0 | y == 0)
+{
+    Console.WriteLine("не принадлежит конкретной четверти");
+}
+if (x > 0 & y > 0)
+{
+    Console.WriteLine("1 плоскость");
+}
+if (x > 0 & y < 0)
+{
+    Console.WriteLine("2 плоскость");
+}
+if (x < 0 & y < 0)
+{
+    Console.WriteLine("3 плоскость");
+}
+if (x < 0 & y > 0)
+{
+    Console.WriteLine("4 плоскость");
+}
