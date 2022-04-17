bc34e53861420e308d4e09e852dd1363c96cd1c7
DanilaPestov <89147750075@mail.ru>
2022-04-17 18:08:52 +1000

    02

---
+++ 02/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите номер четверти");
+int num;
+num = int.Parse(Console.ReadLine());
+if (num == 1)
+{
+    Console.WriteLine("x = от 0 до бесконечности, у = от 0 до бесконечности");
+}
+else if (num == 2)
+{
+    Console.WriteLine("x = от 0 до бесконечности, у = от 0 до - бесконечности");
+}
+else if (num == 3) 
+{
+    Console.WriteLine("x = от - бесконечности до 0, у = от - бесконечности до 0");
+}
+else if (num == 4) 
+{
+    Console.WriteLine("x = от - бесконечности до 0, у = от 0 до бесконечности");
+}
+else 
+{
+    Console.WriteLine("четверть не существует");
+}
